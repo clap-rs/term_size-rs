7fd9eb280e8b4e93b572aef9190e30fb27213e09
Ryo Yamashita <qryxip@gmail.com>
2019-04-27 18:50:24 +0900

    WIP: Implement `dimensions_std[in|err]

--- src/platform/windows.rs
+++ src/platform/windows.rs
@@ -1,19 +1,20 @@
+use winapi::shared::minwindef::DWORD;
 use winapi::um::processenv::GetStdHandle;
-use winapi::um::winbase::STD_OUTPUT_HANDLE;
+use winapi::um::winbase::{STD_ERROR_HANDLE, STD_INPUT_HANDLE, STD_OUTPUT_HANDLE};
 use winapi::um::wincon::GetConsoleScreenBufferInfo;
 use winapi::um::wincon::{CONSOLE_SCREEN_BUFFER_INFO, COORD, SMALL_RECT};
 
 /// Query the current processes's output, returning its width and height as a
-/// number of characters. 
+/// number of characters.
 ///
 /// # Errors
-/// 
+///
 /// Returns `None` if the output isn't to a terminal.
-/// 
+///
 /// # Example
-/// 
+///
 /// To get the dimensions of your terminal window, simply use the following:
-/// 
+///
 /// ```no_run
 /// # use term_size;
 /// if let Some((w, h)) = term_size::dimensions() {
@@ -23,42 +24,20 @@ use winapi::um::wincon::{CONSOLE_SCREEN_BUFFER_INFO, COORD, SMALL_RECT};
 /// }
 /// ```
 pub fn dimensions() -> Option<(usize, usize)> {
-    let null_coord = COORD { X: 0, Y: 0 };
-    let null_smallrect = SMALL_RECT {
-        Left: 0,
-        Top: 0,
-        Right: 0,
-        Bottom: 0,
-    };
-
-    let stdout_h = unsafe { GetStdHandle(STD_OUTPUT_HANDLE) };
-    let mut console_data = CONSOLE_SCREEN_BUFFER_INFO {
-        dwSize: null_coord,
-        dwCursorPosition: null_coord,
-        wAttributes: 0,
-        srWindow: null_smallrect,
-        dwMaximumWindowSize: null_coord,
-    };
-
-    if unsafe { GetConsoleScreenBufferInfo(stdout_h, &mut console_data) } != 0 {
-        Some(((console_data.srWindow.Right - console_data.srWindow.Left + 1) as usize,
-                (console_data.srWindow.Bottom - console_data.srWindow.Top + 1) as usize))
-    } else {
-        None
-    }
+    query(STD_INPUT_HANDLE)
 }
 
 /// Query the current processes's output, returning its width and height as a
 /// number of characters. Returns `None` if the output isn't to a terminal.
 ///
 /// # Errors
-/// 
+///
 /// Returns `None` if the output isn't to a terminal.
-/// 
+///
 /// # Example
-/// 
+///
 /// To get the dimensions of your terminal window, simply use the following:
-/// 
+///
 /// ```no_run
 /// # use term_size;
 /// if let Some((w, h)) = term_size::dimensions() {
@@ -67,20 +46,52 @@ pub fn dimensions() -> Option<(usize, usize)> {
 ///     println!("Unable to get term size :(")
 /// }
 /// ```
-pub fn dimensions_stdout() -> Option<(usize, usize)> { dimensions() }
+pub fn dimensions_stdout() -> Option<(usize, usize)> {
+    query(STD_OUTPUT_HANDLE)
+}
 
 /// This isn't implemented for Windows
-/// 
+///
 /// # Panics
-/// 
+///
 /// This function `panic!`s unconditionally with the `unimplemented!`
 /// macro
-pub fn dimensions_stdin() -> Option<(usize, usize)> { unimplemented!() }
+pub fn dimensions_stdin() -> Option<(usize, usize)> {
+    query(STD_INPUT_HANDLE)
+}
 
 /// This isn't implemented for Windows
-/// 
+///
 /// # Panics
-/// 
+///
 /// This function `panic!`s unconditionally with the `unimplemented!`
 /// macro
-pub fn dimensions_stderr() -> Option<(usize, usize)> { unimplemented!() }
+pub fn dimensions_stderr() -> Option<(usize, usize)> {
+    query(STD_ERROR_HANDLE)
+}
+
+fn query(hdl: DWORD) -> Option<(usize, usize)> {
+    let null_coord = COORD { X: 0, Y: 0 };
+    let null_smallrect = SMALL_RECT {
+        Left: 0,
+        Top: 0,
+        Right: 0,
+        Bottom: 0,
+    };
+
+    let hdl = unsafe { GetStdHandle(hdl) };
+    let mut console_data = CONSOLE_SCREEN_BUFFER_INFO {
+        dwSize: null_coord,
+        dwCursorPosition: null_coord,
+        wAttributes: 0,
+        srWindow: null_smallrect,
+        dwMaximumWindowSize: null_coord,
+    };
+
+    if unsafe { GetConsoleScreenBufferInfo(hdl, &mut console_data) } != 0 {
+        Some(((console_data.srWindow.Right - console_data.srWindow.Left + 1) as usize,
+                (console_data.srWindow.Bottom - console_data.srWindow.Top + 1) as usize))
+    } else {
+        None
+    }
+}
