61276e501acf1f55fa4ddbe92210a4c6b3674b40
Igor Matuszewski <Xanewok@gmail.com>
2019-12-12 02:30:52 +0100

    Fix off-by-one error in Windows screen calculation
    Prior art:
    - https://github.com/softprops/termsize/blob/10ea56487415e14190ff623895d58e7e9a809bbe/src/win.rs#L35-L36
    - https://github.com/eminence/terminal-size/blob/10222f9aeca3c5a1d097df23ee77db0ae8f2a208/src/windows.rs#L34-L35
    
    Also tested using `cmd.exe` and the new values match Layout > Window Size dimensions as expected

--- src/platform/windows.rs
+++ src/platform/windows.rs
@@ -41,8 +41,8 @@ pub fn dimensions() -> Option<(usize, usize)> {
     };
 
     if unsafe { GetConsoleScreenBufferInfo(stdout_h, &mut console_data) } != 0 {
-        Some(((console_data.srWindow.Right - console_data.srWindow.Left) as usize,
-                (console_data.srWindow.Bottom - console_data.srWindow.Top) as usize))
+        Some(((console_data.srWindow.Right - console_data.srWindow.Left + 1) as usize,
+                (console_data.srWindow.Bottom - console_data.srWindow.Top + 1) as usize))
     } else {
         None
     }
